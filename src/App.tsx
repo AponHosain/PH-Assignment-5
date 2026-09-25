e3de10f15ff6c26ef262b363a67eb5eef3136e4b
Apon-vai <aponhossain25@gmail.com>
2026-09-25 17:08:00 +0600

    Added CardTypes interface for props typing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@
 import Nav from './components/Nav';
 import Secction1 from './components/Secction1';  
 
+import type { Icard } from './types/CardTypes';
+
 import Footer from './components/footer';
+
+
+
+const cardFetch = async (): Promise<Icard[]> => {
+  const res = await fetch("/card.json");
+  const data = await res.json();
+  return data;
+};
 
 
 function App() {
---
+++ src/types/CardTypes.tsx
@@ -0,0 +1,12 @@
+export interface Icard {
+  name: string;
+  img : string;
+  description: string;
+  category: string;
+  level: string;
+  rating: number;
+  badge: string;
+}
+
+{/* 1. icon+ badge dibo  */}
+          
